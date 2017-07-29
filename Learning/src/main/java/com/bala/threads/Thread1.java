ab8ab72894f2941f6705375d459fee48ecad8c60
balaji-s <balajisundar.s@gmail.com>
2017-07-29 22:30:38 +0530

    adding something

--- Learning/src/main/java/com/bala/threads/Thread1.java
+++ Learning/src/main/java/com/bala/threads/Thread1.java
@@ -28,7 +28,7 @@ public class Thread1 implements Runnable {
     public static void main( String[] args )    {
 
 
-       int [][]  arrays = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15,16,17},{15,19}};
+       int [][]  arrays = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15,16,17},{15,19},{23,34}};
 
        for(int [] array : arrays){
          Thread thread = new Thread(new Thread1(array));
