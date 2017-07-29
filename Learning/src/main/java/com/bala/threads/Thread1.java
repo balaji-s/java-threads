2fb1c21f732bda97b25155e446b191094729bbb8
balaji-s <balajisundar.s@gmail.com>
2017-07-29 22:25:42 +0530

    committing one example of thread

--- Learning/src/main/java/com/bala/threads/Thread1.java
+++ Learning/src/main/java/com/bala/threads/Thread1.java
@@ -1,9 +1,41 @@
 package com.bala.threads;
-public class Thread1 {
+public class Thread1 implements Runnable {
+
+    private int []  arrays;
+
+    private static void sum(int [] array){
+      
+            int sum = 0;
+            for(int i: array){
+
+                sum+=i;
+            }
+            System.out.println(Thread.currentThread().getName()+":"+sum);
+
+        
+    }
+    public void run(){
+        sum(arrays);
+    }
+
+    public Thread1(int[] arrays){
+
+        this.arrays=arrays;
+
+    }
 
    
     public static void main( String[] args )    {
-        System.out.println( "  World!" );
+
+
+       int [][]  arrays = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15,16,17},{15,19}};
+
+       for(int [] array : arrays){
+         Thread thread = new Thread(new Thread1(array));
+         thread.start();
+       }
+       
+        
     }
 
 }
