a8de3568bd10301f7e446f8782cd010e31bb4c82
balaji-s <balajisundar.s@gmail.com>
2017-07-30 11:44:42 +0530

    committing thread1.java and .classpath

--- Learning/src/main/java/com/bala/threads/Thread1.java
+++ Learning/src/main/java/com/bala/threads/Thread1.java
@@ -4,13 +4,14 @@ public class Thread1 implements Runnable {
     private int []  arrays;
 
     private static void sum(int [] array){
-      
+      long startTime = System.currentTimeMillis();
             int sum = 0;
             for(int i: array){
 
                 sum+=i;
             }
-            System.out.println(Thread.currentThread().getName()+":"+sum);
+
+            System.out.println(Thread.currentThread().getName()+":"+(System.currentTimeMillis()-startTime)+":"+sum);
 
         
     }
@@ -30,12 +31,14 @@ public class Thread1 implements Runnable {
 
        int [][]  arrays = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15,16,17},{15,19},{23,34}};
 
-       for(int [] array : arrays){
+       /*for(int [] array : arrays){
          Thread thread = new Thread(new Thread1(array));
          thread.start();
-       }
+       }*/
        
-        
+        for(int [] array:arrays){
+            sum(array);
+        }
     }
 
 }
